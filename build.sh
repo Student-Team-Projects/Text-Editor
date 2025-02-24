7e458c4bfd11a284e0a251a69b4fa951e60f4b65
Piotr Białek <p.bialek@student.uj.edu.pl>
2025-02-24 14:36:26 +0100

    ninja is unneeded

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 mkdir build
 cd build
-cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 .. -DCMAKE_BUILD_TYPE=Debug -G Ninja
-cmake --build .
+cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 .. -DCMAKE_BUILD_TYPE=Release
+make -j$(nproc)
 cd ..
