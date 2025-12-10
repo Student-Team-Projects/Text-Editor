6a0379d49d6103e379d1bc2652913f7d1cab9825
lynx9687 <michal.manka@student.uj.edu.pl>
2025-12-10 09:52:30 +0100

    Debug build and error log.

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 mkdir build
 cd build
-cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 .. -DCMAKE_BUILD_TYPE=Release
+cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 .. -DCMAKE_BUILD_TYPE=Debug
 cmake --build . -j$(nproc)
 cd ..
